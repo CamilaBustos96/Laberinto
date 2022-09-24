a33f2907efaae3d2607d4f6700c3dcc27dde85df
CamilaBustos96 <camilabustos9696@gmail.com>
2022-09-23 22:38:09 -0300

    comienzolaberinto

--- src/scenes/PantallaLaberintoUno.js
+++ src/scenes/PantallaLaberintoUno.js
@@ -10,12 +10,22 @@ export default class PantallaLaberintoUno extends Phaser.Scene
 
 	preload()
     {
-        
+        this.load.image("festival", "assets/Mapas/Tiles/FestivalesStardew.png")
+        this.load.image("cuevas", "assets/Mapas/Tiles/CuevasStardew.png")
+        this.load.image("otoño", "assets/Mapas/Tiles/OtoñoStardew.png")
+        this.load.tilemapTiledJSON("labe1", "assets/Mapas/LaberintoTerror1.json")
     }
 
     create()
     {
-       
+       const map1 = this.make.tilemap({ key : "label1"});
+
+       const tileset1 = map1.addTilesetImage("FestivalesStardew", "festival");
+       const tileset2 = map1.addTilesetImage("CuevasStardew", "cuevas");
+       const tileset3 = map1.addTilesetImage("OtoñoStardew", "otoño");
+
+        
+
     }
 
     update()
--- src/scenes/PantallaParque.js
+++ src/scenes/PantallaParque.js
@@ -10,12 +10,28 @@ export default class PantallaParque extends Phaser.Scene
 
 	preload()
     {
-        
+        this.load.image("pantparck", "assets/Interfases/Pantallas/Inter_Parque.png")
+        this.load.image("blabe1", "assets/Interfases/Botones/Laberinto1.png")
+        this.load.image("blabe2", "assets/Interfases/Botones/Laberinto2.png")
+        this.load.image("blabe3", "assets/Interfases/Botones/Laberinto3.png")
+
     }
 
     create()
     {
-       
+        this.add.image(0, 0, 'pantparck').setOrigin(0, 0).setScale(0.21, 0.28)
+
+        var botonlaberinto1 = this.add.image(100, 300, "blabe1" ).setOrigin(0, 0).setScale(0.1, 0.15)
+        botonlaberinto1.setInteractive()
+        botonlaberinto1.on("pointerdown", () => this.scene.start('pantalla-laberinto-uno') );
+
+        var botonlaberinto2 = this.add.image(400, 300, "blabe2" ).setOrigin(0, 0).setScale(0.1, 0.15)
+        botonlaberinto2.setInteractive()
+        botonlaberinto2.on("pointerdown", () => this.scene.start('pantalla-laberinto-dos') );
+
+        var botonlaberinto3 = this.add.image(700, 300, "blabe3" ).setOrigin(0, 0).setScale(0.1, 0.15)
+        botonlaberinto3.setInteractive()
+        botonlaberinto3.on("pointerdown", () => this.scene.start('pantalla-laberinto-tres') );
     }
 
     update()
